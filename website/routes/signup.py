bf491b603699f2583265dd7a2ae6f113cd687c0c
ThatOrdinaryDev <sussysu1001@gmail.com>
2026-04-04 15:00:29 -0400

    Changes

---
+++ website/routes/signup.py
@@ -0,0 +1,15 @@
+from flask_wtf import FlaskForm
+from wtforms import EmailField, PasswordField, SelectField, StringField, SubmitField
+from wtforms.validators import DataRequired, EqualTo, Length
+
+
+class SignupForm(FlaskForm):
+    name = StringField("Username", validators=[DataRequired()])
+    first_name = StringField("First Name", validators=[DataRequired()])
+    last_name = StringField("Last Name", validators=[DataRequired()])
+    email = EmailField("Email", validators=[DataRequired()])
+    password = PasswordField(
+        "Password", validators=[DataRequired(), Length(min=8, max=100)]
+    )
+    password2 = PasswordField("Confirm Password", validators=[EqualTo("password")])
+    submit = SubmitField("Sign Up")
--- website/views.py
+++ website/views.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, render_template, request, redirect, url_for
+from sql import execute_query
 
 
 views = Blueprint('views', __name__, template_folder="template")
@@ -8,11 +9,37 @@ def home():
     return render_template("home.html")
 
 @views.route('/browse-page')
-def browse():
-    return render_template("browse.html")
+def browsePage():
+    plants = []
+    
+    try:
+        from sql import execute_query
+        # Fetch plants from database
+        query = "SELECT * FROM plants LIMIT 12"
+        plants = execute_query(query, fetch="all")
+    except Exception as e:
+        print(f"Database error: {e}")
+        plants = []
+    
+    # If no plants in database, use dummy data
+    if not plants:
+        plants = [
+            {'id': i, 'name': 'Snake Plant', 'price': 25.00, 'image': 'homeBG.jpg'}
+            for i in range(1, 7)
+        ]
+    
+    return render_template("browsePage.html", plants=plants)
 
-@views.route('/contact-page')
-def contact():
+@views.route('/contact-page', methods=['GET', 'POST'])
+def contactPage():
+    if request.method == 'POST':
+        user_name = request.form.get('name')
+        user_email = request.form.get('email')
+        user_message = request.form.get('user_message')
+        query = "INSERT INTO Contact_Submissions(name, email, messageText) VALUES (%s, %s, %s)"
+        params = (user_name, user_email, user_message)
+        execute_query(query, params, fetch='none')
+        return redirect(url_for('views.emailConfirmPage'))
     return render_template("contact.html")
 
 @views.route('/check-out-page')
@@ -25,7 +52,7 @@ def detail():
 
 @views.route('/log-in-page')
 def login():
-    return render_template("login.html")
+    return render_template("logIn.html")
 
 @views.route('/sign-up-page')
 def signup():
